120df16ec3a3a5e49683895848837bb6689d08d2
hotmailbelike <fishilike.fish@gmail.com>
2019-10-30 08:45:32 +0600

    Task 1

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* Add your script here */
+$("#list").click(function() {
+  $.ajax({
+    url: "http://skeleton.mernbook.com/api/users",
+    method: "GET",
+    success: function(response) {
+      response.forEach(element => {
+        let temp = $("<p>").text(element.name);
+        $("#users").append(temp);
+      });
+    }
+  });
+  // .done(function(response) {
+  //   // console.log(response);
+  // response.forEach(element => {
+  //   let temp = $("<p>").text(element.name);
+  //   $("#users").append(temp);
+  // });
+  // })
+  // .fail(function(response) {
+  //   console.log(response);
+  // });
+});
